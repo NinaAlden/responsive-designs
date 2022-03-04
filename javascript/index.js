9c02f4955021ef064b613a68f76f3c9b57a4faf0
Nina Aldén <nina.alden@hotmail.com>
2022-03-04 15:43:41 +0100

    final commit
    adding javascript folder and content.
    making layout adjustments
    adding closebtn
    resolving problem with layout on large screen
    fixing classes
    adding overlay + menu

---
+++ javascript/index.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("overlay-menu").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("overlay-menu").style.width = "0%";
+  }
